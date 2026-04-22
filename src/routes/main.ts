96ee444926d7568aea2448fffdae12fc99a18905
Blueperry646 <yanmpenteado@gmail.com>
2026-04-22 08:40:30 -0300

    Criando User

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,19 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post('/user', async (requestAnimationFrame, res) =>{
+    const user = await prisma.user.create({
+        data: {
+            name: "John Name",
+            email: "johnemail@john.com"
+        }
+    })
+    
+    res.json(user)
+} )
