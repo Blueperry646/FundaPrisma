2bf32b89209d95157ddac18d6a3bbf6211d53958
Blueperry646 <yanmpenteado@gmail.com>
2026-05-04 08:36:33 -0300

    Modificando Criação de Usuario

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma';
+import { createUser} from '../services/user'
 
 export const mainRouter = Router();
 
@@ -7,12 +8,10 @@ mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
 
-mainRouter.post('/user', async (requestAnimationFrame, res) =>{
-    const user = await prisma.user.create({
-        data: {
-            name: "John Name",
-            email: "johnemail@john.com"
-        }
+mainRouter.post('/user', async (req, res) =>{
+    const user = await createUser({
+            name: 'John Name',
+            email: 'johnemail@john.com'
     })
     
     res.json(user)
